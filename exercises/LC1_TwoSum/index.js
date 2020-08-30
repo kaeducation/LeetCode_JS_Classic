1e20ce2df06ff7bf72b7fe585bd80ae594e313ce
ddot727 <ddot727@gmail.com>
2020-08-30 14:33:26 -0400

    LC1 Two Sum done

--- exercises/LC1_TwoSum/index.js
+++ exercises/LC1_TwoSum/index.js
@@ -6,6 +6,20 @@
 // twoSum([2,7,11,15], 9) --> [0,1]
 // twoSum([3,5,4], 9) --> [1,2]
 
-function twoSum(arr, target) {}
+// loop through arr
+// loop through again
+// if i + j = target 
+// return [i,j] 
+
+function twoSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length + 1; j++) {
+      if (arr[i] + arr[j] == target) {
+        return [i, j]
+      }
+    }
+  }
+}
+
 
 module.exports = twoSum;
