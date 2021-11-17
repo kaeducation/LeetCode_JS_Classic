4949c3c0906422a6118d03444dce710f16642f65
Shakim Harris <amenraprod@gmail.com>
2021-11-17 02:48:46 -0500

    initial commit

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,23 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+  /* Sanitize input string to only look
+  for alphanumeric characters
+  */
+  s.toLowerCase().replace(/[\W_]/g, "");
+
+  /*
+  Create a left and right pointer
+  */
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  console.log("true");
+}
 
 module.exports = isPalindrome;
